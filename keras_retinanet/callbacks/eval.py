948821f8d850c9a784fd45ae8ef837932241007a
Mohammad Rahimzadeh <56186750+mhrah7495@users.noreply.github.com>
2020-04-06 05:06:21 +0430

    Update eval.py

--- keras_retinanet/callbacks/eval.py
+++ keras_retinanet/callbacks/eval.py
@@ -26,7 +26,7 @@ class Evaluate(keras.callbacks.Callback):
         self,
         generator,
         iou_threshold=0.5,
-        score_threshold=0.05,
+        score_threshold=0.5,
         max_detections=100,
         save_path=None,
         tensorboard=None,

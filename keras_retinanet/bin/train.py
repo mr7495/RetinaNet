302831ef25bd096d24276c9398b5bac96b1e418f
Mohammad Rahimzadeh <56186750+mhrah7495@users.noreply.github.com>
2020-04-22 20:39:26 +0430

    Update train.py

--- keras_retinanet/bin/train.py
+++ keras_retinanet/bin/train.py
@@ -120,8 +120,8 @@ def create_models(backbone_retinanet, num_classes, weights, multi_gpu=0,
     # compile model
     training_model.compile(
         loss={
-            'regression'    : losses.smooth_l1(),
-            'classification': losses.focal()
+            'regression_submodel'    : losses.smooth_l1(),
+            'classification_submodel': losses.focal()
         },
         optimizer=keras.optimizers.adam(lr=lr, clipnorm=0.001)
     )

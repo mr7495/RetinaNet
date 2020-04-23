12554840b23e329f6066f68e968850d60f2009bc
Mohammad Rahimzadeh <56186750+mhrah7495@users.noreply.github.com>
2020-04-23 15:51:19 +0430

    Update __init__.py

--- keras_retinanet/models/__init__.py
+++ keras_retinanet/models/__init__.py
@@ -57,6 +57,8 @@ def backbone(backbone_name):
         from .resnet import ResNetBackbone as b
     elif 'xception' in backbone_name:
         from .xception import XceptionBackbone as b
+    elif 'nasnet' in backbone_name:
+        from .nasnet import nasnetBackbone as b
     elif 'mobilenet' in backbone_name:
         from .mobilenet import MobileNetBackbone as b
     elif 'vgg' in backbone_name:

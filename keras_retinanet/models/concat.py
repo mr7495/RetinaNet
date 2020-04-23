e77bdf0354f708fb4e7afbcc2ec2f2cb662e0300
Mohammad Rahimzadeh <56186750+mhrah7495@users.noreply.github.com>
2020-04-23 19:37:01 +0430

    Update concat.py

--- keras_retinanet/models/concat.py
+++ keras_retinanet/models/concat.py
@@ -70,9 +70,9 @@ def concat_retinanet(num_classes, backbone='concat', inputs=None, modifier=None,
         resnet = modifier(resnet)
 
     # create the full model
-    #vgg_layer_names = ["block3_pool", "block4_pool", "block5_pool"]
-    #resnet_layer_names=[]
-    #layer_outputs = [vgg.get_layer(name).output for name in vgg_layer_names]
-    #layer_outputs.append(resnet.output)
-    layer_outputs=[vgg.output,resnet.output]
+    vgg_layer_names = ["block3_pool", "block4_pool", "block5_pool"]
+    resnet_layer_names=["conv3_block4_out","conv4_bloack6_out","conv5_block3_out"]
+    layer_outputs = [vgg.get_layer(name).output for name in vgg_layer_names]
+    for name in resnet_layer_names:
+        layer_outputs.append(resnet.get_layer(name))
     return retinanet.retinanet(inputs=inputs, num_classes=num_classes, backbone_layers=layer_outputs, **kwargs)

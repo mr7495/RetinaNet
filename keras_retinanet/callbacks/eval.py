443b18475d89cfcce4a39cd03260d8c4426afaa2
Mohammad Rahimzadeh <56186750+mhrah7495@users.noreply.github.com>
2020-05-02 14:34:29 +0430

    Update eval.py

--- keras_retinanet/callbacks/eval.py
+++ keras_retinanet/callbacks/eval.py
@@ -60,7 +60,7 @@ class Evaluate(keras.callbacks.Callback):
         logs = logs or {}
 
         # run evaluation
-        average_precisions, f1_score, accuracy = evaluate(
+        average_precisions, f1_score, accuracy,recall,precision = evaluate(
             self.generator,
             self.model,
             iou_threshold=self.iou_threshold,
